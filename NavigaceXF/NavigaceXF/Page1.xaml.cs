7a66fae3a055995d2f6b460d125f0d641fe2e4ea
Vyhnálek Václav <vyhnalek.vaclav.2018@skola.ssps.cz>
2021-10-19 14:52:31 +0200

    Přidejte soubory projektu.

---
+++ NavigaceXF/NavigaceXF/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigaceXF
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TabbedPage1());
+        }
+
+        private void BackToMainPage_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ NavigaceXF/NavigaceXF/TabbedPage1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigaceXF
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToPage1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void BackToPage2_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void BackToPage3_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+
+        }
+    }
+}
